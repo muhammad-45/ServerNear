c4253d142cd4d750681878e6f3533999845b1290
Muhammad Saad Afridi <180671650+muhammad-45@users.noreply.github.com>
2026-06-07 12:25:14 +0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: ['http://localhost:5173', 'https://researchnest.me'],
   credentials: true
 }));
 app.use(express.json());
